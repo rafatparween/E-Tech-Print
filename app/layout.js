a8aaaae8971bd2693ff1b8416c89d9b87296c8b2
rafatparween <rafatparween27392@gmail.com>
2025-01-28 22:55:17 +0530

    hp printer support

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "eztechprint",
+  title: "HP-Printer Support",
   description: "HP-Printer Support",
 };
 
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-     
       </body>
     </html>
   );
